78b0cfdfbf3e5e511c6250bfe97078ef6890c98c
CSamboni <elementrixz512@gmail.com>
2018-06-25 12:04:49 -0500

    Initial commit

---
+++ src/Photos.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default Photos extends React.Component{
+    render(){
+        return(
+            <div>
+                <div id="anymod-mldrn">
+                  
+                </div>
+            </div>
+        );
+    }
+}
